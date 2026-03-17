711c01be6166ae7803b6f58193db3bd53d18eee6
Moohn <44336373+Moohn5@users.noreply.github.com>
2026-03-17 20:30:57 +0100

    Refactor Gait class and streamline member variables

--- include/Gait.hpp
+++ include/Gait.hpp
@@ -2,13 +2,11 @@
 #include <vector>
 #include <cmath>
 
-// Structure pour stocker une position [Y, Z] d'une patte
 struct Point {
     double y;
     double z;
 };
 
-// Type pour une "pose" complète (les 4 pattes)
 using Pose = std::vector<Point>;
 
 class Gait {
@@ -20,36 +18,32 @@ public:
     static constexpr int RIGHT = 1;
 
 protected:
-    double step_y_func(int leg, int step, int step_count, double leg_step_width, double leg_origin, int fb);
-    double step_z_func(int step, int step_count, double z_origin, double leg_step_height);
+    double step_y_func(int step, int step_count, double leg_step_width, double leg_origin, int fb) {
+        double theta = step * M_PI / (step_count - 1);
+        return leg_origin + (leg_step_width * (std::cos(theta) - fb) / 2.0 * fb);
+    }
+
+    double step_z_func(int step, int step_count, double z_origin, double leg_step_height) {
+        return z_origin - (leg_step_height * step / (step_count - 1));
+    }
 };
 
 class Trot : public Gait {
 public:
     Trot(int fb, int lr);
     std::vector<Pose> get_coords();
-
 private:
     int fb, lr;
     double z_origin = 80.0;
-    double leg_step_height = 20.0;
-    std::vector<double> leg_origin;
-    std::vector<double> leg_step_width;
-    std::vector<double> section_length;
-    std::vector<double> step_down_length;
+    std::vector<double> leg_origin, leg_step_width, section_length, step_down_length;
 };
 
 class Walk : public Gait {
 public:
     Walk(int fb, int lr);
     std::vector<Pose> get_coords();
-
 private:
     int fb, lr;
     double z_origin = 80.0;
-    double leg_step_height = 20.0;
-    std::vector<double> leg_origin;
-    std::vector<double> leg_step_width;
-    std::vector<double> section_length;
-    std::vector<double> step_down_length;
+    std::vector<double> leg_origin, leg_step_width, section_length, step_down_length;
 };
